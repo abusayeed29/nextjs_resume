00766c29b9456a0554114bc14bcf9bfcf4325ddd
Sayeed <md.sayeed@Mds-MacBook-Pro.local>
2021-10-20 18:16:11 +0600

    updated blog

--- pages/blog.js
+++ pages/blog.js
@@ -74,13 +74,59 @@ const blog = ({posts}) => {
 
                         }
                         </div>
-                        <div className="">
-                            {
-                                posts && posts.map( (post, index) => {
-                                <p>{post.tag_name}</p>
-                                })
-                            }
-                        </div>
+                        <aside className="hidden xl:block xl:pl-8">
+                            <h2 class="sr-only">Details</h2>
+                            <div class="space-y-5">
+                                <div class="flex items-center space-x-2">
+                                    <span class="font-display text-4xl font-extrabold text-cool-indigo-600">Free</span>
+                                </div>
+                            </div>
+                            <div className="mt-6 border-t border-gray-200 pt-6 space-y-5">
+                                <ul className="space-y-4">
+                                    <li className="flex items-start lg:col-span-1">
+                                        <div class="flex-shrink-0">
+                                            <svg class="h-5 w-5 text-teal-400" x-description="Heroicon name: check-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                            </svg>
+                                        </div>
+                                        <p class="ml-3 text-base text-teal-700 font-medium">
+                                        Well documented
+                                        </p>
+                                    </li>
+                                    <li className="flex items-start lg:col-span-1">
+                                        <div class="flex-shrink-0">
+                                            <svg class="h-5 w-5 text-teal-400" x-description="Heroicon name: check-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                            </svg>
+                                        </div>
+                                        <p class="ml-3 text-base text-teal-700 font-medium">
+                                        Well documented
+                                        </p>
+                                    </li>
+                                    <li className="flex items-start lg:col-span-1">
+                                        <div class="flex-shrink-0">
+                                            <svg class="h-5 w-5 text-teal-400" x-description="Heroicon name: check-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                            </svg>
+                                        </div>
+                                        <p class="ml-3 text-base text-teal-700 font-medium">
+                                        Well documented
+                                        </p>
+                                    </li>
+                                    <li className="flex items-start lg:col-span-1">
+                                        <div class="flex-shrink-0">
+                                            <svg class="h-5 w-5 text-teal-400" x-description="Heroicon name: check-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                            </svg>
+                                        </div>
+                                        <p class="ml-3 text-base text-teal-700 font-medium">
+                                        Well documented
+                                        </p>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </aside>
                     </div>
                 
                     
