22c54054e694172f6eed5839a9d31567a0ea26bd
Sayeed <md.sayeed@Mds-MacBook-Pro.local>
2021-10-30 18:15:33 +0600

    updated portfolio page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -30,21 +30,18 @@ const portfolio = ({ projects }) => {
                 <section class="text-center bg-gradient-to-r from-gray-200 to-gray-300 py-20">
                     <h1 class="mb-2 text-2xl font-bold text-gray-700 lg:text-4xl mt-10 ">Portfolio </h1>
                     <div class="text-white">
-                        <span class="text-purple-600">Home -</span> Portfolio
+                        <span class="text-red-600">Home -</span> Portfolio
                     </div>
                 </section>
 
                 <div class="flex justify-center mt-8 space-x-2 flex-wrap">
-                    <button className="text-purple-500 bg-white px-10 py-2 shadow-md rounded-full font-medium my-1 hover:shadow-xl active:scale-90 transition duration-150">All</button>
-                    <button className="text-purple-500 bg-white px-10 py-2 shadow-md rounded-full font-medium my-1 hover:shadow-xl active:scale-90 transition duration-150">React</button>
-                    <button className="text-purple-500 bg-white px-10 py-2 shadow-md rounded-full font-medium my-1 hover:shadow-xl active:scale-90 transition duration-150">Laravel</button>
-                    <button className="text-purple-500 bg-white px-10 py-2 shadow-md rounded-full font-medium my-1 hover:shadow-xl active:scale-90 transition duration-150">WordPress</button>
+                    <button className="text-gray-500 bg-white px-10 py-2 shadow-md rounded-full font-medium my-1 hover:shadow-xl active:scale-90 transition duration-150">All</button>
+                    <button className="text-gray-500 bg-white px-10 py-2 shadow-md rounded-full font-medium my-1 hover:shadow-xl active:scale-90 transition duration-150">React</button>
+                    <button className="text-gray-500 bg-white px-10 py-2 shadow-md rounded-full font-medium my-1 hover:shadow-xl active:scale-90 transition duration-150">Laravel</button>
+                    <button className="text-gray-500 bg-white px-10 py-2 shadow-md rounded-full font-medium my-1 hover:shadow-xl active:scale-90 transition duration-150">WordPress</button>
                 </div>
 
                 <section class="container mx-auto grid grid-cols-1 md:grid-cols-3 space-y-2 lg:space-y-0 gap-8 py-10 pb-40">
-
-
-                    {console.log(projects)}
 
                     {projects && projects.map((project, index) => {
                         return (
@@ -67,12 +64,8 @@ const portfolio = ({ projects }) => {
                             </a>
                         )
                     })
+
                     }
-
-
-
-
-
 
                 </section>
 
